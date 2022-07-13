4f887d2c34ba2c09635e6233d2296d863849b31b
nekrashevich.andrei <nekrashevich.andrei@gmail.com>
2022-07-14 00:32:55 +0300

    First implementation.

---
+++ TetrisApp/Tetris/BlockQueue.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Tetris.Blocks;
+
+namespace Tetris
+{
+    public class BlockQueue
+    {
+        private readonly Block[] blocks = new Block[]
+        {
+            new IBlock(),
+            new JBlock(),
+            new LBlock(),
+            new OBlock(),
+            new SBlock(),
+            new TBlock(),
+            new ZBlock(),
+        };
+
+        private readonly Random random = new();
+
+        public Block NextBlock { get; private set; }
+
+        public BlockQueue()
+            => NextBlock = RandomBlock();
+
+        private Block RandomBlock()
+            => blocks[random.Next(blocks.Length)];
+
+        public Block GetAndUpdate()
+        {
+            Block block = NextBlock;
+
+            do
+            {
+                NextBlock = RandomBlock();
+            }
+            while (block.Id == NextBlock.Id);
+
+            return block;
+        }
+    }
+}
---
+++ TetrisApp/Tetris/Blocks/Block.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+
+namespace Tetris.Blocks
+{
+    public abstract class Block
+    {
+        protected abstract Position[][] Tiles { get; }
+
+        protected abstract Position StartOffset { get; }
+
+        public abstract int Id { get; }
+        private int rotationState;
+        private readonly Position offset;
+
+        public Block()
+        {
+            offset = new Position(StartOffset.Row, StartOffset.Column);
+        }
+
+        public IEnumerable<Position> TilePositions()
+        {
+            foreach (var p in Tiles[rotationState])
+            {
+                yield return new Position(p.Row + offset.Row, p.Column + offset.Column);
+            }
+        }
+
+        public void RotateClockWise()
+        {
+            rotationState = (rotationState + 1) % Tiles.Length;
+        }
+
+        public void RotateCounterClockWise()
+        {
+            if (rotationState == 0)
+            {
+                rotationState = Tiles.Length - 1;
+            }
+            else
+            {
+                rotationState--;
+            }
+        }
+
+        public void Move(int rows, int columns)
+        {
+            offset.Row += rows;
+            offset.Column += columns;
+        }
+
+        public void Reset()
+        {
+            rotationState = 0;
+            offset.Row = StartOffset.Row;
+            offset.Column = StartOffset.Column;
+        }
+    }
+}
